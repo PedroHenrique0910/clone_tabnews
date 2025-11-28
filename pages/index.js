930ed79872bf357efa5a366332fa828b76145d6b
Pedro Henrique <132857723+PedroHenrique0910@users.noreply.github.com>
2025-11-28 23:20:42 +0000

    Adicionar imagem na pagina e titulo diferente

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-    return <h1>Rogério Ceni é um dos maiores goleiros da história do Brasil!</h1>
+  return (
+    <div>
+      <h1>Você gostou dessa contratação?</h1>
+      <img 
+        src="https://sports.sbt.com.br/_next/image?url=https%3A%2F%2Fsbt-sports-assets-prod.s3.sa-east-1.amazonaws.com%2FRigoni_1_bf7299b35e.jpg&w=1920&q=90" 
+        alt="contratação" 
+        style={{ width: "300px" }} 
+      />
+    </div>
+  );
 }
 
 export default Home;
