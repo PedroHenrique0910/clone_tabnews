28279159f51ea050336ad7f6b1fc1d4e988764f8
Pedro Henrique <132857723+PedroHenrique0910@users.noreply.github.com>
2025-12-03 23:14:50 +0000

    adiciona scripts lint:checkelint:fix

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone_tabnews
+
 Minha implementação do tabnews.com.br
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,10 @@ function Home() {
   return (
     <div>
       <h1>Você gostou dessa contratação?</h1>
-      <img 
-        src="https://sports.sbt.com.br/_next/image?url=https%3A%2F%2Fsbt-sports-assets-prod.s3.sa-east-1.amazonaws.com%2FRigoni_1_bf7299b35e.jpg&w=1920&q=90" 
-        alt="contratação" 
-        style={{ width: "300px" }} 
+      <img
+        src="https://sports.sbt.com.br/_next/image?url=https%3A%2F%2Fsbt-sports-assets-prod.s3.sa-east-1.amazonaws.com%2FRigoni_1_bf7299b35e.jpg&w=1920&q=90"
+        alt="contratação"
+        style={{ width: "300px" }}
       />
     </div>
   );
