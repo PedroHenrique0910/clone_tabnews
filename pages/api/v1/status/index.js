3d295efe91fadd11295db534f41883b5e03a97f6
Pedro Henrique <132857723+PedroHenrique0910@users.noreply.github.com>
2026-04-12 13:51:46 +0000

    finish /api/v1/status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,34 @@
 import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ message: "Messi é o goat do futebol" });
+  const updatedAt = new Date()
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query("SHOW max_connections;");
+  const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
+
+
+  const dataBaseName = process.env.POSTGRES_DB;  
+
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dataBaseName]
+  });
+
+  const dataBaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_At: updatedAt,
+    dependencies: {
+      database:{
+        version: databaseVersionValue,
+        max_Connections: parseInt(databaseMaxConnectionsValue),
+        opened_Connections: dataBaseOpenedConnectionsValue,
+      }
+    }
+  });
 }
 
 export default status;
