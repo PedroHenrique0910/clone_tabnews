4cb1127c824b1c9c8a44822e31363731f5596245
Pedro Henrique <132857723+PedroHenrique0910@users.noreply.github.com>
2026-08-10 08:50:07 +0000

    fix prettier style linting

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,33 +1,36 @@
 import database from "infra/database";
 
 async function status(request, response) {
-  const updatedAt = new Date()
+  const updatedAt = new Date();
 
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
-  const databaseMaxConnectionsResult = await database.query("SHOW max_connections;");
-  const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
 
-
-  const dataBaseName = process.env.POSTGRES_DB;  
+  const dataBaseName = process.env.POSTGRES_DB;
 
   const databaseOpenedConnectionsResult = await database.query({
     text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-    values: [dataBaseName]
+    values: [dataBaseName],
   });
 
-  const dataBaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count;
+  const dataBaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
 
   response.status(200).json({
     updated_At: updatedAt,
     dependencies: {
-      database:{
+      database: {
         version: databaseVersionValue,
         max_Connections: parseInt(databaseMaxConnectionsValue),
         opened_Connections: dataBaseOpenedConnectionsValue,
-      }
-    }
+      },
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -9,20 +9,17 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
- 
+
   // test updated_At is defined and is a valid date string
 
   const parsedUpdatedAt = new Date(responseBody.updated_At).toISOString();
   expect(responseBody.updated_At).toEqual(parsedUpdatedAt);
 
-  
-  // test version  
+  // test version
   expect(responseBody.dependencies.database.version).toEqual("16.0");
-
 
   // test max_Connections and opened_Connections
 
   expect(responseBody.dependencies.database.max_Connections).toEqual(100);
   expect(responseBody.dependencies.database.opened_Connections).toEqual(1);
-
 });
